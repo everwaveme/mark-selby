9045c8bfaaaa34cb4c7f587a21146c1ed3ddc83f
Farhad Sadykov <f.sadykov@yahoo.com>
2023-06-09 00:21:42 +0300

    finished slider for facts section + small fixes

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
 
   //Carousel for 'facts' section
-  $('.js-slider').slick();
-
+  $('.js-slider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnDotsHover: true,
+    // mobileFirst: true,
+    adaptiveHeight: true,
+    speed: 1000,
+  });
 });
