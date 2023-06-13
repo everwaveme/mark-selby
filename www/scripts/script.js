a357e90157c8bf05dd26db5ced2764eb66649ef3
Farhad Sadykov <f.sadykov@yahoo.com>
2023-06-13 23:59:42 +0300

    changed text in facts slider

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
 
   //Carousel for 'facts' section
   $('.js-slider').slick({
+    // arrows: false,
     dots: true,
     autoplay: true,
     autoplaySpeed: 5000,
+    // fade: true,
     pauseOnDotsHover: true,
-    // mobileFirst: true,
     adaptiveHeight: true,
     speed: 1000,
   });
