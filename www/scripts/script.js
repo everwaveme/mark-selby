2904c6ad93765f4cfcc2ed485feb8783da79c9c6
Farhad Sadykov <f.sadykov@yahoo.com>
2023-06-28 20:49:05 +0300

    small changes

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
 
   //Carousel for 'facts' section
   $('.js-slider').slick({
-    // arrows: false,
     dots: true,
     autoplay: true,
     autoplaySpeed: 5000,
-    // fade: true,
     pauseOnDotsHover: true,
     adaptiveHeight: true,
     speed: 1000,
   });
 });
+
+//AOS animation
+AOS.init();
