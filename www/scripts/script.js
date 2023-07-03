1b8f0abb56078824cc53942619162b1490f4233c
Farhad Sadykov <f.sadykov@yahoo.com>
2023-07-03 23:22:57 +0300

    completed website development

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,41 +1,8 @@
 $(document).ready(function () {
 
-  //Carousel for 'facts' section
-  $('.js-slider').slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnDotsHover: true,
-    // adaptiveHeight: true,
-    speed: 1000,
-
-  //   centerMode: true,
-  //   centerPadding: '60px',
-  //   slidesToShow: 1,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //        arrows: false,
-  //        centerMode: true,
-  //         centerPadding: '40px',
-  //         slidesToShow: 1
-  //       }
-  //    },
-  //    {
-  //      breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: '40px',
-  //         slidesToShow: 1
-  //       }
-  //    }
-  //  ]
-  });
 });
 
 //AOS animation
 AOS.init({
-  // once: true,
+  once: true,
 });
